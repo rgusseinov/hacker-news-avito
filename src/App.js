8f6c2724fd3a554b876275efc7c5a0f8983a1423
Ruslan Gusseinov <r.gusseinov@mail.ru>
2021-12-07 00:17:30 +0600

    feat: add content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ const App = () => {
   return (
     <Box className="wrapper">
       <Header />
+
+      <Box>
+        <div className="box-rating">1.</div>
+        <div className="box-content">
+          <h2>PostgreSQL extension for Oracle DBMS_JOB compatibility</h2>
+          <div className="box-footer">
+            <ul>
+              <li>32 points</li>
+              <li>Author</li>
+              <li>140 comments</li>
+              <li>9 hours ago</li>
+            </ul>
+          </div>
+        </div>
+        <div className="box-action">
+          <p>Link</p>
+        </div>
+      </Box>
     </Box>
   );
 };
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, Container, Link, Toolbar, Typography } from '@material-ui/core';
 import Logo from '../../assets/logo.png';
 import '../../scss/components/header.sass';
 
 const Header = () => {
   return (
     <header className="header">
-      <AppBar>
-        <Toolbar>
-          <img src={Logo} alt="Logo" />
-          <Typography className="logoContainer"> Hacker News </Typography>
-        </Toolbar>
+      <AppBar title={<img src={Logo} />} className="appBar">
+        <Container maxWidth="lg">
+            <Toolbar className="toolbar">
+              <Box className="logo">
+                <img src={Logo} alt="Logo" />
+                <Typography className="title"> Hacker News </Typography>
+              </Box>
+              <nav className="menu">
+                <Link variant="button" className="menu-item" href="#"> News </Link>
+                <Link variant="button" className="menu-item" href="#"> Pasts </Link>
+                <Link variant="button" className="menu-item" href="#"> Comments </Link>
+                <Link variant="button" className="menu-item active" href="#"> Log In </Link>            
+              </nav>
+            </Toolbar>
+        </Container>
       </AppBar>
     </header>
   );
