90411c5884f21259b1871a7cab7367e87ea5d2b4
= <r.@mail.ru>
2021-12-07 21:25:35 +0600

    feat: work on news html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core';
+import { Box, Container } from '@material-ui/core';
 import React from 'react';
 import Header from './components/header/header';
 import './scss/app.sass';
@@ -7,24 +7,62 @@ const App = () => {
   return (
     <Box className="wrapper">
       <Header />
+      <br/><br/><br/><br/>
 
-      <Box>
-        <div className="box-rating">1.</div>
-        <div className="box-content">
-          <h2>PostgreSQL extension for Oracle DBMS_JOB compatibility</h2>
-          <div className="box-footer">
-            <ul>
-              <li>32 points</li>
-              <li>Author</li>
-              <li>140 comments</li>
-              <li>9 hours ago</li>
-            </ul>
+      <Container maxWidth="lg">
+        <Box className="news-item">
+          <div className="news-item__box-rating">
+            <div className="rating up">1</div>
           </div>
-        </div>
-        <div className="box-action">
-          <p>Link</p>
-        </div>
-      </Box>
+          <div className="news-item__box-content">
+            <h2>PostgreSQL extension for Oracle DBMS_JOB compatibility</h2>
+            <div className="news-item__box-footer">
+              <ul>
+                <li>32 points</li>
+                <li>Author</li>
+                <li>140 comments</li>
+                <li>9 hours ago</li>
+              </ul>
+            </div>
+          </div>          
+        </Box>
+
+        <Box className="news-item">
+          <div className="news-item__box-rating">
+            <div className="rating up">1</div>
+          </div>
+          <div className="news-item__box-content">
+            <h2>PostgreSQL extension for Oracle DBMS_JOB compatibility</h2>
+            <div className="news-item__box-footer">
+              <ul>
+                <li>32 points</li>
+                <li>Author</li>
+                <li>140 comments</li>
+                <li>9 hours ago</li>
+              </ul>
+            </div>
+          </div>          
+        </Box>
+
+
+        <Box className="news-item">
+          <div className="news-item__box-rating">
+            <div className="rating up">1</div>
+          </div>
+          <div className="news-item__box-content">
+            <h2>PostgreSQL extension for Oracle DBMS_JOB compatibility</h2>
+            <div className="news-item__box-footer">
+              <ul>
+                <li>32 points</li>
+                <li>Author</li>
+                <li>140 comments</li>
+                <li>9 hours ago</li>
+              </ul>
+            </div>
+          </div>          
+        </Box>
+
+      </Container>
     </Box>
   );
 };
