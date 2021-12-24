583867450440a37fea570be371f638e4f6deef0f
Ruslan Gusseinov <r.gusseinov@mail.ru>
2021-12-24 20:31:16 +0600

    refactor: revert links

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container">
         <div className="row header-row">
           <div className="logo">
-            <Link to="/hacker-news-avito/" className="logo-link">
+            <Link to="/" className="logo-link">
               <img src={Logo} alt="Logo" className="logo-img" />
               <span className="logo-text"> Hacker News </span>
             </Link>
